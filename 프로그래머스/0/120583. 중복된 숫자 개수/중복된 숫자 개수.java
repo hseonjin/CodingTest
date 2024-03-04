99a4d8a1222c0fb88156890474d55a1824a10f5a
Seonjin Heo <126320580+hseonjin@users.noreply.github.com>
2024-03-04 19:03:50 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for (int arr : array) {
+            if (arr == n) answer += 1;
+        }
+        
+        return answer;
+    }
+}
