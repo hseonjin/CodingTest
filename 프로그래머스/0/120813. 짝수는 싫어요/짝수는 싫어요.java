95d1fec1747016fc4c6e15772c9652611a91c3d5
Seonjin Heo <126320580+hseonjin@users.noreply.github.com>
2024-03-05 20:26:29 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i=1; i<=n; i+=2) {
+            arr.add(i);
+        }
+        
+        int[] answer = new int[arr.size()];
+        for (int i=0; i<arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
