1d1246429dabd5503337914514bd2539a4620e9a
Seonjin Heo <126320580+hseonjin@users.noreply.github.com>
2024-03-09 00:12:10 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.35 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length-5];
+        Arrays.sort(num_list);
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
+    }
+}
