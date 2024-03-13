fcb1c26c9a432cb7c588c688b5133a6a8a70a853
Seonjin Heo <126320580+hseonjin@users.noreply.github.com>
2024-03-13 17:28:10 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder(myString.toLowerCase());
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) == 'a') {
-                sb.setCharAt(i, 'A');
-            }
-        }
-        return sb.toString();
+        myString = myString.toLowerCase().replace("a", "A");
+        return myString;
     }
 }
