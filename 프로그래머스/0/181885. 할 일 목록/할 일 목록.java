f4df30f4ffe207367c2c3135123dc941447f8422
Seonjin Heo <126320580+hseonjin@users.noreply.github.com>
2024-03-13 17:13:15 +0900

    [level 0] Title: 할 일 목록, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i=0; i<todo_list.length; i++) {
+            if (!finished[i]) arr.add(todo_list[i]); 
+        }
+        
+        String[] answer = arr.toArray(new String[0]);
+        
+        return answer;
+    }
+}
